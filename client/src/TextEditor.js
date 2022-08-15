9fcb76ce675349939f53485d8807d0ac29429dd9
Jonjo7Dia <jonosphgames@gmail.com>
2022-08-15 16:02:46 -0300

    server side added

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -1,7 +1,7 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect } from "react";
 import Quill from "quill";
 import "quill/dist/quill.snow.css";
-
+import {io} from 'socket.io-client';
 const TOOLBAR_OPTIONS = [
     [{header: [1,2,3,4,5,6, false]}],
     [{font: []}],
@@ -14,6 +14,14 @@ const TOOLBAR_OPTIONS = [
     ['clean'],
 ];
 export default function TextEditor() {
+ useEffect(()=>{
+     const socket = io('http://localhost:3001');
+
+     return ()=>{
+         socket.disconnect
+     }
+ }, []);
+
   const wrapperRef = useCallback((wrapper) => {
     if (wrapper === null) return;
     wrapper.innerHTML = "";
